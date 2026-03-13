dd9596b1a6c0ee970bd04c30e81bc1367d864794
Janani <18jananim@gmail.com>
2026-03-13 09:46:25 +0530

    Add files via upload

---
+++ ValidAnagram.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+      if(s.length()!=t.length())
+      {
+        return false;
+      }  
+      char[] ch=s.toCharArray();
+      char[] ch1=t.toCharArray();
+      Arrays.sort(ch);
+      Arrays.sort(ch1);
+      return Arrays.equals(ch,ch1);
+
+
+    }
+}
